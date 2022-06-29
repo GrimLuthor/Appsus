a25203a887201586cf7ac98269472c0d9e56987f
GrimLuthor <daniel.olenev@gmail.com>
2022-06-29 12:34:54 +0300

    commit

--- js/apps/mail/js/pages/mail-app.cmp.js
+++ js/apps/mail/js/pages/mail-app.cmp.js
@@ -4,7 +4,7 @@ export default {
 `,
 data() {
     return {
-        email: {
+        email: [{
             id: 'e101',
             subject: 'Miss you!',
             body: 'Would love to catch up sometimes',
@@ -12,6 +12,17 @@ data() {
             sentAt : 1551133930594,
             to: 'momo@momo.com'
         },
+        {
+            id: 'e102',
+            subject: 'Miss you!',
+            body: 'Would love to catch up sometimes',
+            isRead: false,
+            sentAt : 1551133930594,
+            to: 'momo@momo.com'
+        }
+        ],
+
+
         loggedinUser: {
             email: 'user@appsus.com',
             fullname: 'Mahatma Appsus'
