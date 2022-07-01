40a0d1018518a6213a44cf9ec0d36b4dfc58404f
GrimLuthor <daniel.olenev@gmail.com>
2022-07-01 10:19:47 +0300

    a bit of css

--- js/apps/mail/js/cmps/email-folder-list.cmp.js
+++ js/apps/mail/js/cmps/email-folder-list.cmp.js
@@ -3,16 +3,34 @@
 export default {
     template: `
         <div class="sidebar-folders">
-            <p @click="changeFolder('all')">All</p>
-            <p @click="changeFolder('inbox')">Inbox</p>
-            <p @click="changeFolder('sent')">Sent</p>
-            <p @click="changeFolder('draft')">Drafts</p>
+            <p class="folder-tag" :class="{folderselect : allSelected}" @click="changeFolder('all')">All</p>
+            <p class="folder-tag" :class="{folderselect : inboxSelected}" @click="changeFolder('inbox')">Inbox</p>
+            <p class="folder-tag" :class="{folderselect : sentSelected}" @click="changeFolder('sent')">Sent</p>
+            <p class="folder-tag" :class="{folderselect : draftSelected}" @click="changeFolder('draft')">Drafts</p>
         </div>
     `,
+    data() {
+        return {
+            allSelected: true,
+            inboxSelected: false,
+            sentSelected: false,
+            draftSelected: false
+        }
+    },
     methods: {
         changeFolder(folder){
             console.log('changeFolder', folder);
             this.$emit('changeFolder', folder);
+            
+            this.allSelected = false;
+            this.inboxSelected = false;
+            this.sentSelected = false;
+            this.draftSelected = false;
+
+            if(folder ===  'all') this.allSelected = true;
+            if(folder === 'inbox') this.inboxSelected = true;
+            if(folder === 'sent') this.sentSelected = true;
+            if(folder === 'draft') this.draftSelected = true;
         }
     },
 }
